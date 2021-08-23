95dac5b2d1cf358ed8fca9dffffeaa11553f5838
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-08-23 11:40:41 -0700

    building my schema tables

---
+++ main/schema.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+USE employees_db;
+
+-- Start Building DEPARTMENT Table
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+-- Start Building ROLE Table
+CREATE TABLE role (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id),
+    REFERENCES department(id) 
+    ON DELETE SET NULL
+
+);
+
+-- Start Building EMPLOYEE Table
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT,
+PRIMARY KEY (id),
+FOREIGN KEY (role_id) REFERENCES role(id) OON DELETE SET NULL,
+FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
+
+
