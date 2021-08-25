4f3db9f7bb97fe44650033412d1e4b8963b81d48
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-08-24 18:09:24 -0700

    test 3 for seeds after setting value to null

--- main/schema.sql
+++ main/schema.sql
@@ -4,13 +4,13 @@ CREATE DATABASE employees_DB;
 USE employees_DB;
 -- Start Building DEPARTMENT Table
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 -- Start Building ROLE Table
 CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT,
@@ -19,7 +19,7 @@ CREATE TABLE role (
 );
 -- Start Building EMPLOYEE Table
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
--- main/seeds.sql
+++ main/seeds.sql
@@ -12,22 +12,22 @@ VALUES
 (2, "Operations Supervisor", 40000, 2),
 (3, "Human Resources Manager", 45000, 3), 
 (4, "Human Resources Supervisor", 40000, 4),
-(5, "Training Manager", 40000, 5), 
-(6, "Training Supervisor", 35000, 6),
-(7, "Sales Manager", 60000, 7), 
-(8, "Sales Supervisor", 50000, 8);
+(5, "Training Manager", 40000, 1), 
+(6, "Training Supervisor", 35000, 2),
+(7, "Sales Manager", 60000, 3), 
+(8, "Sales Supervisor", 50000, 4);
 
 -- Seeding Data into EMPLOYEE TABLE
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES
-(1, "Sam", "Smith", 1, 2), 
+(1, "Sam", "Smith", 2, NULL), 
 (4, "Hanz", "Zimmer", 4, NULL), 
-(3, "Ismail", "Khan", 3, 3), 
-(5, "Ahmed", "Abid", 6, 1),
-(2, "Mark", "Hamill", 7, NULL), 
+(3, "Ismail", "Khan", 6, NULL), 
+(5, "Ahmed", "Abid", 8, NULL),
+(2, "Mark", "Hamill", 4, NULL), 
 (8, "Tom", "Riddle", 8, NULL),
-(7, "Darth", "Vader", 5, NULL), 
-(6, "Tony", "Stank", 2, 2);
+(7, "Darth", "Vader", 2, NULL), 
+(6, "Tony", "Stank", 6, NULL);
 
 -- SELECT department_id "Department Code", role_id
 -- COUNT(*) "No of Employees",
